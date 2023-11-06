b01dc5c86c9a1db4cbab15f8f862289cc963d770
Dex REAU <dev@oqva.digital>
2023-11-06 10:54:19 +0000

    Update consts.ts

---
+++ src/consts.ts
@@ -0,0 +1,13 @@
+
+// META
+
+export const SITE_TITLE = "Lorem";
+
+export const SITE_DESCRIPTION = "Ipsum";
+
+export const SITE_KEYWORDS = "Dolor"
+    
+
+// SOCIAL SHARING IMAGE
+
+export const OG_IMAGE = '/og/social-image.jpg'
