07e0a1d19625f5fb38c7c5d155a972eb25ae650d
Dex REAU <dev@oqva.digital>
2023-11-06 02:27:11 +0000

    First Up

---
+++ src/scripts/description-carousel.js
@@ -0,0 +1,67 @@
+let bullets = document.querySelectorAll('.intro .descriptions .bullet')
+
+let carouselBts = document.querySelectorAll('.roulette .icons button')
+
+let rouletteIcons = document.querySelector('.roulette .icons')
+let spinContainers = document.querySelectorAll('.roulette .icons .spin_ctnr')
+
+
+
+
+let counter = 0;
+setTimeout(() => {
+    bullets[counter].classList.add('show')
+}, 900);
+
+var repeatHighlight = setInterval(() => {
+    
+    if(document.querySelector('.intro .descriptions .bullet.show')) {
+        document.querySelector('.intro .descriptions .bullet.show').classList.remove('show')
+    }
+
+    setTimeout(() => {
+        bullets[counter].classList.add('show')
+    }, 200);
+
+    if(counter != bullets.length - 1) {
+        counter++
+    } else {
+        counter = 0;
+    }
+}, 6000);
+
+
+
+function alternateBullet() {
+
+    for(let i=0;i<carouselBts.length;i++) {
+        carouselBts[i].addEventListener('click', function() {
+            document.querySelector('.intro .descriptions .bullet.show').classList.remove('show')
+
+            setTimeout(() => {
+                bullets[i].classList.add('show')
+            }, 200);
+
+            clearInterval(repeatHighlight)
+        })
+        
+        carouselBts[i].addEventListener('mouseover', function() {
+            rouletteIcons.classList.add('stop')
+
+            for(let j=0;j<spinContainers.length;j++) {
+                spinContainers[j].classList.add('stop')
+            }
+        })
+        
+        carouselBts[i].addEventListener('mouseout', function() {
+            rouletteIcons.classList.remove('stop')
+
+            for(let j=0;j<spinContainers.length;j++) {
+                spinContainers[j].classList.remove('stop')
+            }
+        })
+    }
+
+}
+
+alternateBullet()
